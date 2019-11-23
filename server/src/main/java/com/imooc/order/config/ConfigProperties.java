b126833550f31d22f5c35c33f69b3d0c55eae57b
wangpeng <wangpeng0717@sina.cn>
2019-11-23 14:26:38 +0800

    ConfigurationProperties嵌套子类使用

--- server/src/main/java/com/imooc/order/config/ConfigProperties.java
+++ server/src/main/java/com/imooc/order/config/ConfigProperties.java
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Component;
 public class ConfigProperties {
     private String name;
     private String age;
+    private Type type;
 }
---
+++ server/src/main/java/com/imooc/order/config/Type.java
@@ -0,0 +1,9 @@
+package com.imooc.order.config;
+
+import lombok.Data;
+
+@Data
+public class Type {
+    private Integer red;
+    private String blue;
+}
