9069a56d7675469545c4d5cce717f60403595004
wangpeng116 <wangpeng116@longfor.com>
2019-11-28 22:04:30 +0800

    请求体入参

--- server/src/main/java/com/imooc/order/controller/WifeController.java
+++ server/src/main/java/com/imooc/order/controller/WifeController.java
@@ -1,7 +1,9 @@
 package com.imooc.order.controller;
 
 import com.imooc.order.config.ConfigProperties;
+import com.imooc.order.form.OrderForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +16,10 @@ public class WifeController {
     @RequestMapping("/print")
     public String print(){
         return configProperties.toString();
+    }
+
+    @RequestMapping("/catchRequestBody")
+    public String catchRequestBody(@RequestBody OrderForm orderForm){
+        return "ok";
     }
 }
