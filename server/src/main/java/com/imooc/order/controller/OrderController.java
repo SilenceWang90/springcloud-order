b4f58812cd69ee68c6d9b1f86acc28e0b6b23eaf
wangpeng116 <wangpeng116@longfor.com>
2019-12-17 14:26:59 +0800

    完结订单

--- server/src/main/java/com/imooc/order/controller/OrderController.java
+++ server/src/main/java/com/imooc/order/controller/OrderController.java
@@ -12,9 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -57,5 +55,14 @@ public class OrderController {
         return ResultVOUtil.success(map);
     }
 
+    /**
+     * 完结订单
+     * @param orderId
+     * @return
+     */
+    @PostMapping("/finish")
+    public ResultVO<OrderDTO> finish(@RequestParam("orderId") String orderId) {
+        return ResultVOUtil.success(orderService.finish(orderId));
+    }
 
 }
