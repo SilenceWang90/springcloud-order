255c98dcdf68cb7b8569aad9ef81f21f6b308405
wangpeng116 <wangpeng116@longfor.com>
2019-12-18 18:26:17 +0800

    配置文件配置

--- server/src/main/java/com/imooc/order/controller/HystrixController.java
+++ server/src/main/java/com/imooc/order/controller/HystrixController.java
@@ -7,6 +7,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,6 +25,11 @@ import java.util.List;
 @Slf4j
 public class HystrixController {
 
+    /**
+     * 基本使用：服务降级
+     *
+     * @return
+     */
     @HystrixCommand(fallbackMethod = "fallback", commandProperties = {
             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
     })
@@ -34,6 +40,12 @@ public class HystrixController {
         return restTemplate.postForObject("http://127.0.0.1:8080/product/listForOrder", paramters, String.class);
     }
 
+
+    /**
+     * 高级使用，通过线程池进行服务降级处理
+     *
+     * @return
+     */
     @HystrixCommand(threadPoolKey = "WPThreadPool", ignoreExceptions = NullPointerException.class, threadPoolProperties = {
             @HystrixProperty(name = "coreSize", value = "1"),
             @HystrixProperty(name = "maxQueueSize", value = "1"),
@@ -45,6 +57,31 @@ public class HystrixController {
         log.info("我再运行");
         return restTemplate.postForObject("http://127.0.0.1:8080/product/listForOrder", paramters, String.class);
     }
+
+
+    /**
+     * 服务熔断
+     *
+     * @return
+     */
+    @HystrixCommand(commandProperties = {
+            //启用熔断
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")
+    })
+    @GetMapping("/prodInfoList")
+    public String prodInfoList(@RequestParam("number") Integer number) {
+        if (number % 2 == 0) {
+            return "success";
+        }
+        RestTemplate restTemplate = new RestTemplate();
+        List<String> paramters = Lists.newArrayList("157875196366160022");
+        log.info("我再运行");
+        return restTemplate.postForObject("http://127.0.0.1:8080/product/listForOrder", paramters, String.class);
+    }
+
 
     private String fallback() {
         return "服务拥挤，请稍后再试";
