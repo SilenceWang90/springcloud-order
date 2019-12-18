41296c71ed56d90e8ba60f65601c96bce2366d57
wangpeng116 <wangpeng116@longfor.com>
2019-12-18 19:41:46 +0800

    feign-hystrix

--- server/src/main/java/com/imooc/order/OrderApplication.java
+++ server/src/main/java/com/imooc/order/OrderApplication.java
@@ -2,7 +2,6 @@ package com.imooc.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
@@ -12,9 +11,8 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.imooc.product.client")
 @EnableCircuitBreaker
-@SpringCloudApplication
 //增加扫描范围，因为jar中的bean需要扫描到的话就需要增加扫描的路径
-@ComponentScan(basePackages = "com.imooc")
+@ComponentScan(basePackages = "com.imooc.product")
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
