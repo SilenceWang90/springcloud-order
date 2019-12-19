682fdd23aac3ee64d77ad4cbb046065fffd999d0
wangpeng116 <wangpeng116@longfor.com>
2019-12-19 11:46:55 +0800

    feign-hystrix

--- server/src/main/java/com/imooc/order/repository/OrderDetailRepository.java
+++ server/src/main/java/com/imooc/order/repository/OrderDetailRepository.java
@@ -2,9 +2,10 @@ package com.imooc.order.repository;
 
 import com.imooc.order.dataobject.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, String> {
 
     List<OrderDetail> findByOrderId(String orderId);
