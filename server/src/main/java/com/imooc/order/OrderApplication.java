32b8320e523cceb0bffc13123972022ff5979261
wangpeng116 <wangpeng116@longfor.com>
2019-12-19 14:56:31 +0800

    hystrix-dashboard

--- server/src/main/java/com/imooc/order/OrderApplication.java
+++ server/src/main/java/com/imooc/order/OrderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableCircuitBreaker
 //增加扫描范围，因为jar中的bean需要扫描到的话就需要增加扫描的路径
 @ComponentScan(basePackages = "com.imooc")
+@EnableHystrixDashboard
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
